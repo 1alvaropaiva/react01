6e4b5dec01cc92971b9bbe3673b947de1d34aff1
1alvaropaiva <alvaropaiva.dev@gmail.com>
2024-08-16 11:25:53 -0300

    dia16

---
+++ src/app/App.tsx
@@ -0,0 +1,8 @@
+import { AppRoutes } from "../app/routes";
+
+export const App = () => {
+  return (
+    <AppRoutes/>
+  );
+}
+
---
+++ src/app/routes/index.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Dashboard } from "../pages";
+
+export const AppRoutes = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/pagina-inicial" element = {<Dashboard/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
