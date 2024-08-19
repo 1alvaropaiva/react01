557c496c258c37ed840993db5ec97561e4ec1c47
1alvaropaiva <alvaropaiva.dev@gmail.com>
2024-08-18 23:36:11 -0300

    dia18

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Dashboard } from "../pages";
-import { Redirect } from "react-router-dom";
 
 export const AppRoutes = () => {
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/pagina-inicial" element = {<Dashboard/>}/>
-                <Route path="*" element = {<Redirect to="/pagina-inicial" />} />
 
             </Routes>
         </BrowserRouter>
